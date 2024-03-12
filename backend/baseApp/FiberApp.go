524c7779168a5751b9b640c2d4ab0c8159e492f4
bbahd30 <b_bonal@cs.iitr.ac.in>
2024-03-12 16:34:30 +0530

    Add User model

--- backend/baseApp/FiberApp.go
+++ backend/baseApp/FiberApp.go
@@ -1,6 +1,8 @@
 package base
 
 import (
+	"backend/apis"
+
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -13,5 +15,18 @@ func FiberApp() *BaseFiberApp {
 	app := &BaseFiberApp{
 		App: fiber.New(),
 	}
+
+	app.registerUserApis()
+
 	return app
+}
+
+func (fiberApp *BaseFiberApp) registerUserApis() {
+	userRouter := fiberApp.App.Group("/user")
+
+	apis.CreateUser(userRouter)
+	apis.GetUser(userRouter)
+	apis.GetUsers(userRouter)
+	apis.UpdateUser(userRouter)
+	apis.DeleteUser(userRouter)
 }
---
+++ backend/models/user_model.go
@@ -0,0 +1,21 @@
+package models
+
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	ID       uuid.UUID `gorm:"type:uuid"`
+	Username string    `json:"username"`
+	Password string    `json:"password"`
+	Name     string    `json:"name"`
+	// Email        string    `json:"email"`
+}
+
+func (user *User) BeforeCreate(tx *gorm.DB) (err error) {
+	user.ID = uuid.New()
+
+	return nil
+}
